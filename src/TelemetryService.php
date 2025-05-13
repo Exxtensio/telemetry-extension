f06da9f254320def24ab77bb466716d5bbf6f3bc
vladlen-beilik <vladlen.beilik@gmail.com>
2025-05-13 08:32:00 +0200

    ref

--- src/TelemetryService.php
+++ src/TelemetryService.php
@@ -2,14 +2,12 @@
 
 namespace Exxtensio\TelemetryExtension;
 
-use OpenTelemetry\API\Trace\SpanKind;
-use OpenTelemetry\API\Trace\TracerInterface;
-use OpenTelemetry\Context\Context;
+use OpenTelemetry\API\Trace;
 use Throwable;
 
 class TelemetryService
 {
-    public function __construct(protected TracerInterface $tracer) {}
+    public function __construct(protected Trace\TracerInterface $tracer) {}
 
     /**
      * @throws Throwable
@@ -17,21 +15,24 @@ class TelemetryService
     public function trace(string $name, callable $callback, array $attributes = []): mixed
     {
         $span = $this->tracer->spanBuilder($name)
-            ->setParent(Context::getRoot())
-            ->setSpanKind(SpanKind::KIND_SERVER)
+            ->setSpanKind(Trace\SpanKind::KIND_SERVER)
             ->startSpan();
 
         foreach ($attributes as $key => $value) {
             $span->setAttribute($key, $value);
         }
 
+        $scope = $span->activate();
+
         try {
-            return $callback();
+            return $callback($span);
         } catch (Throwable $e) {
             $span->recordException($e);
+            $span->setStatus(Trace\StatusCode::STATUS_ERROR, $e->getMessage());
             throw $e;
         } finally {
             $span->end();
+            $scope->detach();
         }
     }
 }
