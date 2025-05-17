8f83fc401ef69a58d12edad1abf13368ff2087f9
vladlen-beilik <vladlen.beilik@gmail.com>
2025-05-17 11:28:38 +0200

    ref

--- src/TelemetryInterface.php
+++ src/TelemetryInterface.php
@@ -2,14 +2,9 @@
 
 namespace Exxtensio\TelemetryExtension;
 
-use OpenTelemetry\API\Trace\SpanInterface;
-use OpenTelemetry\Context\ScopeInterface;
-
 interface TelemetryInterface
 {
     public function startRoot(string $tracerName, string $name, array $attributes = []): void;
     public function endRoot(): void;
-    public function startSpan(string $tracerName, string $name, array $attributes = []): array;
-    public function endSpan(SpanInterface $span, ScopeInterface $scope): void;
-    public function withSpan(string $tracerName, string $name, callable $callback, array $attributes = []): mixed;
+    public function withSpan(string $tracerName, string $name, callable $callback, array $trace = [], array $attributes = []): mixed;
 }
--- src/TelemetryService.php
+++ src/TelemetryService.php
@@ -2,20 +2,19 @@
 
 namespace Exxtensio\TelemetryExtension;
 
-use OpenTelemetry\API\Trace\SpanKind;
-use OpenTelemetry\API\Trace\StatusCode;
-use OpenTelemetry\API\Trace\SpanInterface;
-use OpenTelemetry\API\Trace\TracerProviderInterface;
+use OpenTelemetry\API\Trace;
+use OpenTelemetry\API\Trace\Propagation\TraceContextPropagator;
+use OpenTelemetry\Context\Context;
 use OpenTelemetry\Context\ScopeInterface;
 use Throwable;
 
 class TelemetryService implements TelemetryInterface
 {
-    protected TracerProviderInterface $provider;
-    protected ?SpanInterface $rootSpan = null;
+    protected Trace\TracerProviderInterface $provider;
+    protected ?Trace\SpanInterface $rootSpan = null;
     protected ?ScopeInterface $rootScope = null;
 
-    public function __construct(TracerProviderInterface $provider) {
+    public function __construct(Trace\TracerProviderInterface $provider) {
         $this->provider = $provider;
     }
 
@@ -27,7 +26,6 @@ class TelemetryService implements TelemetryInterface
 
         $tracer = $this->provider->getTracer($tracerName);
         $this->rootSpan = $tracer->spanBuilder($name)
-            ->setSpanKind(SpanKind::KIND_SERVER)
             ->startSpan();
 
         foreach ($attributes as $key => $value) {
@@ -40,6 +38,7 @@ class TelemetryService implements TelemetryInterface
     public function endRoot(): void
     {
         if($this->rootSpan) {
+            $this->rootSpan->setStatus(Trace\StatusCode::STATUS_OK);
             $this->rootSpan->end();
             $this->rootSpan = null;
         }
@@ -50,34 +49,16 @@ class TelemetryService implements TelemetryInterface
         }
     }
 
-    public function startSpan(string $tracerName, string $name, array $attributes = []): array
-    {
-        $tracer = $this->provider->getTracer($tracerName);
-        $span = $tracer->spanBuilder($name)
-            ->setSpanKind(SpanKind::KIND_SERVER)
-            ->startSpan();
-
-        foreach ($attributes as $key => $value) {
-            $span->setAttribute($key, $value);
-        }
-        $scope = $span->activate();
-        return [$span, $scope];
-    }
-
-    public function endSpan(SpanInterface $span, ScopeInterface $scope): void
-    {
-        $span->end();
-        $scope->detach();
-    }
-
     /**
      * @throws Throwable
      */
-    public function withSpan(string $tracerName, string $name, callable $callback, array $attributes = []): mixed
+    public function withSpan(string $tracerName, string $name, callable $callback, array $trace = [], array $attributes = []): mixed
     {
         $tracer = $this->provider->getTracer($tracerName);
+        $context = TraceContextPropagator::getInstance()->extract($trace, null, Context::getCurrent());
+
         $span = $tracer->spanBuilder($name)
-            ->setSpanKind(SpanKind::KIND_INTERNAL)
+            ->setParent($context)
             ->startSpan();
 
         foreach ($attributes as $key => $value) {
@@ -88,11 +69,11 @@ class TelemetryService implements TelemetryInterface
 
         try {
             $response = $callback($span);
-            $span->setStatus(StatusCode::STATUS_OK);
+            $span->setStatus(Trace\StatusCode::STATUS_OK);
             return $response;
         } catch (Throwable $e) {
             $span->recordException($e);
-            $span->setStatus(StatusCode::STATUS_ERROR, $e->getMessage());
+            $span->setStatus(Trace\StatusCode::STATUS_ERROR, $e->getMessage());
             throw $e;
         } finally {
             $span->end();
